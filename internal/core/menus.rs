ca42bc34bc1cb1e554c23f773579f732d51a2bed
U-ALDEBERAN\Nate <npwoods@mess.org>
2026-05-28 19:44:56 -0400

    Removing unnecessary `#[allow(dead_code)]`

--- internal/core/menus.rs
+++ internal/core/menus.rs
@@ -51,7 +51,6 @@ pub struct MenuFromItemTree {
     root: RefCell<SharedVector<MenuEntry>>,
     next_id: Cell<usize>,
     tracker: Pin<Box<PropertyTracker>>,
-    #[allow(dead_code)]
     condition: Option<Pin<Box<Property<bool>>>>,
     visible: Option<Pin<Box<Property<bool>>>>,
 }
